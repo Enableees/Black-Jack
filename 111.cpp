b84c325b8589c76b1ef379ac872f16621e8ff1f2
Kompot24 <efim.popov.2017@bk.ru>
2025-06-19 12:07:13 +0800

    Check cards and fix bug

--- 111.cpp
+++ 111.cpp
@@ -1,61 +1,72 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <Windows.h>
 #include <random>
 #include <map>
-
-using namespace std;
-
-#include <iostream>
-#include <windows.h>
-#include <string>
 #include <ctime>
 #include <cstdlib>
 
 using namespace std;
-
-void randomcart(int amount)
+int check[52][52];
+string randomcart(int amount)
 {
-    SetConsoleOutputCP(CP_UTF8); // Для корректного отображения мастей
-    srand(static_cast<unsigned int>(time(0))); // Инициализация генератора случайных чисел
+    SetConsoleOutputCP(CP_UTF8);
+    srand(time(0));
 
     string carts[13] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
     string suits[4] = { u8"♠", u8"♥", u8"♦", u8"♣" };
-
+    int c;
+    int num = 0;
+    int suitIndex = 0;
     for (int i = 0; i < amount; i++)
     {
-        int num = rand() % 13; // От 0 до 12
-        int suitIndex = rand() % 4;
-
+        while (check[num][suitIndex] == 1)
+        {
+            num = rand() % 13;
+            suitIndex = rand() % 4;
+        }
+        check[num][suitIndex] = 1;
         string value = carts[num];
         string suit = suits[suitIndex];
 
-        // Базовая форма карты
         string cardTemplate =
-            " ______ \n"
-            "|V     |\n"
-            "|      |\n"
-            "|  S   |\n"
-            "|      |\n"
-            "|     V|\n"
-            " ¯¯¯¯¯¯ \n\n";
+            " _______ \n"
+            "|V      |\n"
+            "|       |\n"
+            "|  S    |\n"
+            "|       |\n"
+            "|      V|\n"
+          u8" ¯¯¯¯¯¯¯ ";
 
-        // Замена символов V — на номинал, S — на масть
+        string cardTemplate10 =
+            " _______ \n"
+            "|10     |\n"
+            "|       |\n"
+            "|  S    |\n"
+            "|       |\n"
+            "|     10|\n"
+          u8" ¯¯¯¯¯¯¯ ";
+
         size_t pos;
-        while ((pos = cardTemplate.find('V')) != string::npos)
-            cardTemplate.replace(pos, 1, value);
+        if (value != "10")
+        {
+            while ((pos = cardTemplate.find("V")) != string::npos)
+                cardTemplate.replace(pos, 1, value);
 
-        cardTemplate.replace(cardTemplate.find('S'), 1, suit);
-
-        cout << value + suit << endl;
-        cout << cardTemplate;
+            cardTemplate.replace(cardTemplate.find('S'), 1, suit);
+            return cardTemplate;
+        }
+        else {
+            cardTemplate10.replace(cardTemplate.find('S'), 1, suit);
+            return cardTemplate10;
+        }
     }
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-
-    const string asciiArt = R"(            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    //setlocale(LC_ALL, "Russian");
+    string table = u8R"(            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%                                                                                                             %%%%
        %%%%  _______    _______    _______    _______    _______    _______    _______    _______    _______    _______   %%%%
@@ -66,7 +77,7 @@ int main() {
   %%%%      |       |  |       |  |       |  |       |  |       |  |       |  |       |  |       |  |       |  |       |       %%%%
  %%%%        ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯    ¯¯¯¯¯¯¯         %%%%
 %%%%                                                                                                                             %%%%
-%%%%          Действие бота: f        СТАВКА БОТА: n                             Действие игрока: g      СТАВКА ИГРОКА: m        %%%%
+%%%%                                                                                                                             %%%%
 %%%%                                                           _______                                                           %%%%
 %%%%        dMMMMb     dMP     .aMMMb    .aMMMb     dMP dMP   |\  |  /|            dMMMMMP     .aMMMb    .aMMMb     dMP dMP      %%%%
 %%%%       dMP"dMP    dMP     dMP"dMP   dMP"VMP    dMP.dMP    | \ | / |               dMP     dMP"dMP   dMP"VMP    dMP.dMP       %%%%
@@ -86,9 +97,10 @@ int main() {
          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%)";
 
-    std::cout << asciiArt << std::endl;
+    table.insert(770, randomcart(1));
+    cout << table << endl;
 
-    randomcart(5);
+    //randomcart(52);
 
     return 0;
 }
